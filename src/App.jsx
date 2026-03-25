665cfcef067c164c1ff8b408a1ec9073b0eab28f
Nmanell <enmanuel.bueno@proton.me>
2026-03-25 23:39:09 +0000

    Fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,10 @@ import UserProfile from './pages/UserProfile';
 import CoachSearch from './pages/CoachSearch';
 import MyCoach from './pages/MyCoach';
 import CoachLanding from './pages/CoachLanding';
-<<<<<<< HEAD
 import MoodTrackPage from './pages/MoodTrackPage';
 import WorkoutLogPage from './pages/WorkoutLogPage';
 import SwitchCoach from './pages/SwitchCoach';
 import MealTrackPage from './pages/MealTrackPage';
-=======
-import OnboardingSurvey from './pages/OnboardingSurvey';
->>>>>>> 89bf191 (Onboarding Survey)
 
 function App() {
   return (
@@ -25,7 +21,6 @@ function App() {
       <Routes>
         <Route path="/LoginPage" element={<LoginPage />} />
         <Route path="/RegistrationPage" element={<RegistrationPage />} />
-<<<<<<< HEAD
         <Route path="/LandingPage/:clientId" element={ <ProtectedRoutes> <LandingPage /> </ProtectedRoutes> } />
         <Route path="/UserProfile/:clientId" element={ <ProtectedRoutes> <UserProfile/> </ProtectedRoutes> } />
         <Route path="/CoachSearch/:clientId" element={ <ProtectedRoutes> <CoachSearch/> </ProtectedRoutes> } />
@@ -35,14 +30,6 @@ function App() {
         <Route path="/WorkoutLogPage/:clientId" element={ <ProtectedRoutes> <WorkoutLogPage/> </ProtectedRoutes> } />
         <Route path="/CoachLanding/:clientId" element={ <ProtectedRoutes> <CoachLanding/> </ProtectedRoutes> } />
         <Route path="/SwitchCoach/:clientId" element={ <ProtectedRoutes> <SwitchCoach/> </ProtectedRoutes> } />
-=======
-        <Route path="/LandingPage/:clientId" element={<ProtectedRoutes> <LandingPage /> </ProtectedRoutes>} />
-        <Route path="/UserProfile/:clientId" element={<ProtectedRoutes> <UserProfile /> </ProtectedRoutes>} />
-        <Route path="/OnboardingSurvey/:clientId" element={<OnboardingSurvey />} />
-        <Route path="/CoachSearch/:clientId" element={<ProtectedRoutes> <CoachSearch /> </ProtectedRoutes>} />
-        <Route path="/MyCoach/:clientId" element={<ProtectedRoutes> <MyCoach /> </ProtectedRoutes>} />
-        <Route path="/CoachLanding/:clientId" element={<ProtectedRoutes> <CoachLanding /> </ProtectedRoutes>} />
->>>>>>> 89bf191 (Onboarding Survey)
         <Route path="/" element={<Navigate to="/RegistrationPage" />} />
       </Routes>
     </Router>
