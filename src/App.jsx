aecc834b43c1c0ffdeeafc19836f80c6c4de4eff
nmanel <enmanuel.bueno04@proton.me>
2026-03-26 18:24:08 -0400

    fixed Onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ import MoodTrackPage from './pages/MoodTrackPage';
 import WorkoutLogPage from './pages/WorkoutLogPage';
 import SwitchCoach from './pages/SwitchCoach';
 import MealTrackPage from './pages/MealTrackPage';
-import WorkoutSearchPage from './pages/WorkoutSearchPage';
-import StepsTracker from './pages/StepsTracker';
-import CustomExercise from './pages/CustomExercise';
+import OnboardingSurveyPage from './pages/OnboardingSurvey';
 
 function App() {
   return (
@@ -23,19 +21,17 @@ function App() {
     <Router>
       <Routes>
         <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/OnboardingSurvey/:clientId" element={<OnboardingSurveyPage />} />
         <Route path="/RegistrationPage" element={<RegistrationPage />} />
-        <Route path="/LandingPage/:clientId" element={ <ProtectedRoutes> <LandingPage /> </ProtectedRoutes> } />
-        <Route path="/UserProfile/:clientId" element={ <ProtectedRoutes> <UserProfile/> </ProtectedRoutes> } />
-        <Route path="/CoachSearch/:clientId" element={ <ProtectedRoutes> <CoachSearch/> </ProtectedRoutes> } />
-        <Route path="/MyCoach/:clientId" element={ <ProtectedRoutes> <MyCoach/> </ProtectedRoutes> } />
-        <Route path="/MoodTrackPage/:clientId" element={ <ProtectedRoutes> <MoodTrackPage/> </ProtectedRoutes> } />
-        <Route path="/MealTrackPage/:clientId" element={ <ProtectedRoutes> <MealTrackPage/> </ProtectedRoutes> } />
-        <Route path="/WorkoutLogPage/:clientId" element={ <ProtectedRoutes> <WorkoutLogPage/> </ProtectedRoutes> } />
-        <Route path="/CoachLanding/:clientId" element={ <ProtectedRoutes> <CoachLanding/> </ProtectedRoutes> } />
-        <Route path="/SwitchCoach/:clientId" element={ <ProtectedRoutes> <SwitchCoach/> </ProtectedRoutes> } />
-        <Route path="/WorkoutSearchPage/:clientId" element={ <ProtectedRoutes> <WorkoutSearchPage/> </ProtectedRoutes> } />
-        <Route path="/StepsTracker/:clientId" element={ <ProtectedRoutes> <StepsTracker/> </ProtectedRoutes> } />
-        <Route path="/CustomExercise/:clientId" element={ <ProtectedRoutes> <CustomExercise/> </ProtectedRoutes> } />
+        <Route path="/LandingPage/:clientId" element={<ProtectedRoutes> <LandingPage /> </ProtectedRoutes>} />
+        <Route path="/UserProfile/:clientId" element={<ProtectedRoutes> <UserProfile /> </ProtectedRoutes>} />
+        <Route path="/CoachSearch/:clientId" element={<ProtectedRoutes> <CoachSearch /> </ProtectedRoutes>} />
+        <Route path="/MyCoach/:clientId" element={<ProtectedRoutes> <MyCoach /> </ProtectedRoutes>} />
+        <Route path="/MoodTrackPage/:clientId" element={<ProtectedRoutes> <MoodTrackPage /> </ProtectedRoutes>} />
+        <Route path="/MealTrackPage/:clientId" element={<ProtectedRoutes> <MealTrackPage /> </ProtectedRoutes>} />
+        <Route path="/WorkoutLogPage/:clientId" element={<ProtectedRoutes> <WorkoutLogPage /> </ProtectedRoutes>} />
+        <Route path="/CoachLanding/:clientId" element={<ProtectedRoutes> <CoachLanding /> </ProtectedRoutes>} />
+        <Route path="/SwitchCoach/:clientId" element={<ProtectedRoutes> <SwitchCoach /> </ProtectedRoutes>} />
         <Route path="/" element={<Navigate to="/RegistrationPage" />} />
       </Routes>
     </Router>
