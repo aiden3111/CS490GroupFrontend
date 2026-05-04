81d30fa8639bc37b7fb6f97af12ce055b039cfac
thefire4 <carloscabanilla09@gmail.com>
2026-05-03 20:20:04 -0400

    trying to fix errors within the website

--- src/pages/MessagingPage.jsx
+++ src/pages/MessagingPage.jsx
@@ -6,7 +6,6 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ["websocket", "polling"],
-  withCredentials: true,
 });
 
 const MessagingPage = () => {
