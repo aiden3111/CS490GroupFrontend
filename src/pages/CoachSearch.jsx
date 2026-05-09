c1f5228c4461765729cea1830284665eacba78bc
mdse789 <md789@njit.edu>
2026-05-09 11:39:23 -0400

    fixing coach filters

--- src/pages/CoachSearch.jsx
+++ src/pages/CoachSearch.jsx
@@ -348,7 +348,7 @@ const CoachSearch = () => {
               padding: "8px 16px",
               borderRadius: "6px",
               border: "none",
-              transition: "background-color 0.2s"
+              marginTop: "5px"
             }}
               onClick={() => {
                 setselectedFilters([]);
